e0b23eade088ba0718dabc6eb3851a292c789738
JunhuiShen-Parallel <js2ke@mtmail.mtsu.edu>
2024-11-24 12:54:22 -0600

    test whether it is pushed successful

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 class Config:
     SECRET_KEY = 'Zhouyi_CSCI6180'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:zhouyi1995@localhost:3306/visualization1'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:123456@localhost:3306/visualization'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
